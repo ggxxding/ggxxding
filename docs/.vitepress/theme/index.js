d056d4d595bacec90c367e71b82df60b979a45ca
ggxxding <ggxxding@163.com>
2025-07-18 21:35:01 +0800

    add ElementUI

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,6 +3,9 @@ import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
@@ -12,6 +15,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.use(ElementPlus)
   }
 }
